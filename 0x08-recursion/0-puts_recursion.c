72f573f2b1942eb5acfb954eb0f41b1494abbbc7
PaulOnyejela <paulonyejela@gmail.com>
2023-09-28 16:39:18 -0700

    2nd try

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
- * _puts_recursion - print a string
+ * _puts_recursion - prints string
  * @s:string
  * Return:void
  */
 void _puts_recursion(char *s)
 {
 	while (*s != '\0')
-    {
-        putchar(*s); /* print letter */
-        s = s + 1;  /* point to the next letter. */
-    }
+	{
+		_putchar(*s); /* print letter */
+		s = s + 1;  /* point to the next letter. */
+	}
 	putchar('\n');
-	
 }
